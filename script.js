9807e99f7bcaf896fd5679ef2d83f79612cad20b
Hugo <hugowinged@gmail.com>
2025-03-10 16:15:08 -0700

    Added UI enter name, and score showcase functions

--- script.js
+++ script.js
@@ -4,6 +4,14 @@
     const grid_cells = document.querySelectorAll('.grid_cell');
     const winner_text = document.querySelector('.winner_text');
     const restart_btn = document.querySelector('.restart_btn');
+    const score_reset_btn =  document.querySelector('.score_reset_btn');
+    const player_x = document.querySelector('.player_x');
+    const player_o = document.querySelector('.player_o');
+    const x = prompt("Enter name for player X...");
+    const o = prompt("Enter name for player O...");
+    const score_x = document.querySelector('.score_x');
+    const score_o = document.querySelector('.score_o');
+    let store_scores = JSON.parse(localStorage.getItem("store_scores")) || {player_x: 0, player_o: 0}
 
     const xo_match = [
         [0, 1, 2],
@@ -20,16 +28,23 @@
     let active_player = "X";
     let game_status = false;
 
-    function startGame() {
+    function startGame() {   
+        player_x.textContent = `${x} is X`;
+        player_o.textContent = `${o} is O`;
+
+        score_x.textContent = `${store_scores.player_x}`;
+        score_o.textContent = `${store_scores.player_o}`;
+    
         grid_cells.forEach(cell => cell.addEventListener('click', clickCell));
         restart_btn.addEventListener('click', playAgain);
+        score_reset_btn.addEventListener('click', scoreReset);
         winner_text.textContent = `${active_player}'s turn`;
         game_status = true;
     }
 
 
     function clickCell() {
-        const cellIndex = this.getAttribute('cellIndex');
+        const cellIndex = this.dataset.cellIndex;
 
         if (cell_placeholders[cellIndex] !== "" || !game_status) return;
 
@@ -65,13 +80,24 @@
 
         if (winnerFound) {
             winner_text.textContent = `${active_player} wins!`;
+            if(active_player === "X") {
+                store_scores.player_x++;
+                score_x.textContent = `${store_scores.player_x}`;
+            } else if(active_player === "O") {
+                store_scores.player_o++;
+                score_o.textContent = `${store_scores.player_o}`
+            }
+       
             game_status = false;
         } else if (!cell_placeholders.includes("")) {
             winner_text.textContent = `Draw!`;
             game_status = false;
         } else {
             switchPlayer();
+            return;
         }
+
+        localStorage.setItem("store_scores", JSON.stringify(store_scores));
     }
 
     function playAgain() {
@@ -80,6 +106,14 @@
         winner_text.textContent = `${active_player}'s turn`;
         grid_cells.forEach(cell => cell.textContent = "");
         game_status = true;
+    }
+
+    function scoreReset() {
+        store_scores = {player_x: 0, player_o: 0};
+        score_x.textContent = `${store_scores.player_x}`;
+        score_o.textContent = `${store_scores.player_o}`;
+
+        localStorage.setItem("store_scores", JSON.stringify(store_scores));
     }
 
     startGame();
